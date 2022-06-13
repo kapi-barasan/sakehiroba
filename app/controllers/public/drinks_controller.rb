94148e960ce507a31be6a4c1c42b385e0653cf26
AkiraKimura <kapibarakapibara4649@gmail.com>
2022-06-13 10:06:40 +0000

    add bootstrap

---
+++ app/controllers/public/drinks_controller.rb
@@ -0,0 +1,24 @@
+class Public::DrinksController < ApplicationController
+  
+  def new
+  end
+  
+  def show
+  end
+  
+  def index
+  end
+  
+  def create
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,26 @@
 Rails.application.routes.draw do
-
+  
+  #サーバー起動時の画面
   root to: "homes#top"
 
+  
   devise_for :users,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
   }
+  
+  #ゲストログインの記述
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'public/sessions#guest_sign_in'
+  end  
+
 
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
   }
+  
+  namespace :public do
+    resources :drinks
+  end
 
 end
